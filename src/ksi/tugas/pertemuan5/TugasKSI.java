bbd99d00689b9207f7a44594221625134973570d
Myname <rachmanaldiansyah03@gmail.com>
2023-05-09 11:13:22 +0700

    initial commit

---
+++ src/ksi/tugas/pertemuan5/TugasKSI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ksi.tugas.pertemuan5;
+
+/**
+ * @author
+ * Anggota Kelompok:
+ * 10119028 - Rizal Sihombing
+ * 10119038 - Rachman Aldiansyah
+ * 10119042 - Muhammad Rifqu Abdillah
+ */
+public class TugasKSI {
+
+    public static void main(String[] args) {
+        frmUtama frame_utama = new frmUtama();
+        frame_utama.setVisible(true);
+    }
+    
+}
